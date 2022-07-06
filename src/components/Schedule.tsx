492818d02cdff0a16da5a505554fa7e53f7db396
kexy.huang <kexy.huang@nadisystem.com>
2022-07-06 15:27:40 +0800

    update

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -19,22 +19,30 @@ interface ClickEventTarget extends EventTarget {
 interface TouchEventTarget extends EventTarget {
   id: string;
 }
-
+interface ColorPickTarget extends EventTarget {
+  value: string;
+}
 interface paintDataFromLocal {
   id: string;
-  listData: string[];
+  listData: coordinateData[];
+}
+
+interface coordinateData {
+  coor: string;
+  color: string;
 }
 
 export default () => {
   const wrapRef = useRef<HTMLDivElement>(null);
-  const [list, setList] = useState<string[]>([]);
+  const listPanelRef = useRef<HTMLDivElement>(null);
+  const [list, setList] = useState<coordinateData[]>([]);
   const [showText, setShowText] = useState<boolean>(false);
   const [selectMode, setSelectMode] = useState<Boolean>(true);
   const [prevDataFromLocal, setPrevDataFromLocal] = useState<
     paintDataFromLocal[]
   >([]);
   const [detectList, setDetectList] = useState<DragPoint[]>([]);
-  const [currentColor, setCurrentColor] = useState<string>('');
+  const [currentColor, setCurrentColor] = useState<string>("");
   const [currentPicked, setCurrentPicked] = useState<paintDataFromLocal>();
   // console.log("detectList", detectList);
   // console.log("list", list);
@@ -68,16 +76,16 @@ export default () => {
     });
   });
 
-  //點擊選取
-  const pickCube = (eventTarget: ClickEventTarget) => {
-    if (selectMode) {
-      tempList.push(eventTarget.id);
-      setList(tempList);
-    } else {
-      tempList = R.without([eventTarget.id], list);
-      setList(tempList);
-    }
-  };
+  // //點擊選取
+  // const pickCube = (eventTarget: ClickEventTarget) => {
+  //   if (selectMode) {
+  //     tempList.push({coor:eventTarget.id, color:'#ccff00'});
+  //     setList(tempList);
+  //   } else {
+  //     tempList = R.without([eventTarget.id], list);
+  //     setList(tempList);
+  //   }
+  // };
   let tempList = [...list];
 
   const resetList = () => {
@@ -91,7 +99,8 @@ export default () => {
     let parentRect = wrapRef.current.getBoundingClientRect();
     try {
       return cubeList.filter((cubeItem, cubeIndex) => {
-        let cubeRect = cubeItem.getBoundingClientRect();
+        let cubeRect: DOMRect = cubeItem.getBoundingClientRect();
+        // console.log('cubeRect', cubeRect)
         let cubeLeft = cubeRect.left - parentRect.left;
         let cubeRight = cubeRect.right - parentRect.left;
         let cubeTop = cubeRect.top - parentRect.top;
@@ -111,10 +120,16 @@ export default () => {
     if (detectList.length) {
       let lastChanged = handleReturnCubeId(detectList[detectList.length - 1]);
       if (lastChanged) {
-        if (R.includes(lastChanged, tempList)) {
-          return;
+        if (
+          R.includes(
+            lastChanged,
+            tempList.map((item) => {
+              return item.coor;
+            })
+          )
+        ) {
         } else {
-          tempList.push(lastChanged);
+          tempList.push({ coor: lastChanged, color: "#ccff00" });
           setList(tempList);
         }
       }
@@ -122,27 +137,18 @@ export default () => {
   }, [detectList]);
 
   let temp = [...detectList];
-  const handleTouchMove = (
-    // eventTarget: TouchEventTarget,
-    e: React.TouchEvent
-  ) => {
+  const handleTouchMove = (e: React.TouchEvent) => {
     if (!wrapRef.current) return;
-
-    // console.log("eventTarget", eventTarget);
     let parentRect = wrapRef.current.getBoundingClientRect();
     let clientXX = e.touches[0].clientX - parentRect.left;
-    // console.log('clientXX', clientXX)
     let clientYY = e.touches[0].clientY - parentRect.top;
-    // console.log('clientYY', clientYY)
-    let cubeList = [...document.querySelectorAll(".cube")];
-    let coor = { x: clientXX, y: clientYY }
-    
+    let coor = { x: clientXX, y: clientYY };
+
     if (R.includes(coor, temp)) {
     } else {
       temp.push(coor);
     }
     setDetectList(temp);
-
   };
 
   const renderScheduleCube = () => {
@@ -153,28 +159,23 @@ export default () => {
             return (
               <div
                 className={`cube ${
-                  R.includes(`cube_${key + 1}-${cubeKey + 1}`, list)
+                  R.includes(
+                    `cube_${key + 1}-${cubeKey + 1}`,
+                    list.map((item) => {
+                      return item.coor;
+                    })
+                  )
                     ? "selected"
                     : ""
                 }`}
                 id={`cube_${key + 1}-${cubeKey + 1}`}
                 key={cubeKey}
-                onClick={(e) =>
-                  pickCube({
-                    ...e.target,
-                    id: `cube_${key + 1}-${cubeKey + 1}`
-                  })
-                }
               ></div>
             );
           })}
         </div>
       );
     });
-  };
-
-  const show = () => {
-    setShowText(true);
   };
 
   const save = () => {
@@ -192,94 +193,90 @@ export default () => {
       setDetectList([]);
       setList([]);
     }, 200);
+    setTimeout(() => {
+      if (listPanelRef.current) {
+        listPanelRef.current.scrollTo({
+          top: Number.MAX_SAFE_INTEGER,
+          behavior: "smooth"
+        });
+      }
+    }, 250);
   };
 
   useEffect(() => {
     if (showText) {
       tempList = [];
-      console.log('currentPicked', currentPicked)
+      console.log("currentPicked", currentPicked);
       currentPicked?.listData.forEach((item, key) => {
         setTimeout(() => {
-          tempList.push(item);
+          tempList.push({ coor: item.coor, color: "#ccff00" });
           setList([...tempList]);
         }, 30 * key);
         setCurrentPicked(undefined);
-        setShowText(false)
+        setShowText(false);
       });
     }
   }, [showText]);
 
-  const play = (item:paintDataFromLocal) => {
-    console.log('item', item)
-    console.log('play')
+  const play = (item: paintDataFromLocal) => {
+    console.log("item", item);
+    console.log("play");
     // setList([]);
     setCurrentPicked(item);
     setShowText(true);
-  }
+  };
+  const changeColor = (eventTarget: ColorPickTarget) => {
+    setCurrentColor(eventTarget.value);
+  };
 
   return (
-    <div className="schedule_container">
-      <div className="schedule_body">
+    <div className="pixel_canva_container">
+      <div className="paint_body">
         <div
           className="wrap"
           draggable={true}
           ref={wrapRef}
-          onTouchMove={(e) => 
-            handleTouchMove(e)
-          }
+          onTouchMove={(e) => handleTouchMove(e)}
         >
           {renderScheduleCube()}
         </div>
+        <div className="btn_area">
+          <input
+            type="color"
+            id="head"
+            name="head"
+            value="#ccff66"
+            className="color_picker"
+            onChange={(e) => changeColor(e.target)}
+          />
+          <div className="reset_btn" onClick={resetList}>
+            Reset
+          </div>
+          <div className="show_btn" onClick={save}>
+            Save
+          </div>
+        </div>
       </div>
-      <div className="btn_area">
-        <div className="reset_btn" onClick={resetList}>
-          Reset
-        </div>
-        <div className="show_btn" onClick={show}>
-          Show
-        </div>
-        <div className="show_btn" onClick={save}>
-          Save
-        </div>
-        {/* <div className="mode_switch_panel">
-            <div className="panel_title"></div>
-            <div className="panel_inner">
-              <div className="button_text">開</div>
-              <div
-                className={`mode_switch_button ${selectMode ? 'enable' : ''}`}
-                style={{ backgroundColor: '#42c6fc' }}
-                onClick={() => setSelectMode(true)}
-              ></div>
-              <div
-                className={`mode_switch_button ${!selectMode ? 'disable' : ''}`}
-                style={{ backgroundColor: 'black' }}
-                onClick={() => setSelectMode(false)}
-              ></div>
-              <div className="button_text">關</div>
-            </div>
-          </div> */}
-      </div>
-      <div className="list_panel">
-        {prevDataFromLocal.map((item, index) => {
-          return (
-            <div className="each_row" key={index}>
-              <div className="data_id">{item.id}</div>
-              <div
-                className="delete_btn"
-                onClick={() => deleteThisPaint(item.id)}
-              >
-                delete
+
+      <div className="list_panel" ref={listPanelRef}>
+        <div className="list_panel_inner" >
+          {prevDataFromLocal.map((item, index) => {
+            return (
+              <div className="each_row" key={index}>
+                {/* <div className="data_id">{item.id}</div> */}
+                <div className="data_id">{index + 1}</div>
+
+                <div className="play_btn" onClick={() => play(item)}>
+                  Play
+                </div>
+                <div
+                  className="delete_icon"
+                  onClick={() => deleteThisPaint(item.id)}
+                ></div>
               </div>
-              <div
-                className="play_btn"
-                onClick={() => play(item)}
-              >
-                Play
-              </div>
-              <div></div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
